11a5d098039189f15eb9566dd271ff12466fbc78
clement-gh <64776760+clement-gh@users.noreply.github.com>
2023-02-01 11:57:27 +0100

    img bouton

---
+++ app/src/main/java/com/milhet/translationapp/historiqueActivity.java
@@ -0,0 +1,14 @@
+package com.milhet.translationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class historiqueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historique);
+    }
+}
