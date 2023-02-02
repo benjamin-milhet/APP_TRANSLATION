05c993acf71a3732c1681a09a4d4ad9d6f730b18
clement-gh <64776760+clement-gh@users.noreply.github.com>
2023-02-02 08:59:37 +0100

    design & creation des activity

---
+++ app/src/main/java/com/milhet/translationapp/ParametreActivity.java
@@ -0,0 +1,14 @@
+package com.milhet.translationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ParametreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parametre);
+    }
+}
